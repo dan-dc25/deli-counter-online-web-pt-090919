8a7c4eb5c058543830fb74d1c0bcb9ab0797a2e4
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-20 16:08:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,25 @@
-#def line(array)
-  #phrase = "The line is currently:"
-  #if array == []
-  #  puts "The line is currently empty."
-  #else array.length > 0
-  #  array.each_with_index do |name, index|
-  #  phrase.push(" #{index + 1}. #{name}")
-  #  end
-  #  return phrase
-  #end
-#end
-
-def line(array)
-  katz_deli = []
-  if katz_deli == []
-    puts "The line is currently empty."
-  else katz_deli => 1
-    phrase = "The line is currently:"
-    array.each_with_index do |name, index|
-      phrase = phrase + "#{index + 1} #{name}."
-      katz_deli << phrase
+def line(deli)
+  if deli.empty?
+    puts 'The line is currently empty.'
+  else
+    in_line = 'The line is currently:'
+    deli.each.with_index(1) do |person, i|
+      in_line << " #{i}. #{person}"
     end
-    return katz_deli
+    puts in_line
   end
 end
+
+#def line(array)
+#  katz_deli = []
+#  if katz_deli == []
+#    puts "The line is currently empty."
+#  else katz_deli => 1
+#    phrase = "The line is currently:"
+#    array.each_with_index do |name, index|
+#      phrase = phrase + "#{index + 1} #{name}."
+#      katz_deli << phrase
+#    end
+#    return katz_deli
+#  end
+#end
