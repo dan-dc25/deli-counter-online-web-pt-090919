fc68baa4e56e8a950d1c3508156be59f1f8850be
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-20 16:18:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,16 +10,8 @@ def line(deli)
   end
 end
 
-#def line(array)
-#  katz_deli = []
-#  if katz_deli == []
-#    puts "The line is currently empty."
-#  else katz_deli => 1
-#    phrase = "The line is currently:"
-#    array.each_with_index do |name, index|
-#      phrase = phrase + "#{index + 1} #{name}."
-#      katz_deli << phrase
-#    end
-#    return katz_deli
-#  end
-#end
+def take_a_number(line(deli), person)
+  take_a_number.each_with_index(1) do |person, i|
+    line(deli) << "#{i}. #{person}"
+  end
+end
