cfd403786d2abe29be88b95fc886ad94822732d8
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-20 15:54:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+#def line(array)
+  #phrase = "The line is currently:"
+  #if array == []
+  #  puts "The line is currently empty."
+  #else array.length > 0
+  #  array.each_with_index do |name, index|
+  #  phrase.push(" #{index + 1}. #{name}")
+  #  end
+  #  return phrase
+  #end
+#end
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else
+    phrase = "The line is currently: "
+    array.each_with_index do |name, index|
+    phrase (" #{index + 1}. #{name}")
+  end
+    return phrase
+  end
+end
