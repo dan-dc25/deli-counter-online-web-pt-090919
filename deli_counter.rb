039fca3261129f9a446e448906b052c3e3a92d9d
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-20 18:04:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(array, name)
 end
 
 def now_serving(deli)
-  if deli.empty?
+  if deli == []
     puts "There is nobody waiting to be served!"
   else
       puts "Currently serving #{deli.first}."
