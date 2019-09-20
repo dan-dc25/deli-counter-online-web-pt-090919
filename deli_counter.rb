ecc2c0581f5f2044549c4dfc8aa685c2cbf2a27c
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-20 18:04:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,6 @@ def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    deli.each do |name|
       puts "Currently serving #{deli.first}."
       deli.shift
     end
